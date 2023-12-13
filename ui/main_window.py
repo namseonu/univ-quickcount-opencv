934b030a447cdbf72328364e389d36393b4cfef3
seonwoo <ska2870ghk@naver.com>
2023-12-13 23:39:11 +0900

    feat: Adjust the widget of label and button for better UI
    related to #2

--- ui/main_window.py
+++ ui/main_window.py
@@ -37,10 +37,19 @@ class QuickCount(QMainWindow):
         self.setWindowTitle("Quick Count")
 
         self.lbl_image = QLabel(self)
+        self.lbl_image.hide()
+
         self.lbl_input_by_user = QLabel(self)
+        self.lbl_input_by_user.hide()
+
         self.lbl_answer_by_opencv = QLabel(self)
+        self.lbl_answer_by_opencv.hide()
+
         self.lbl_result = QLabel(self)
+        self.lbl_result.hide()
+
         self.input_by_user = QLineEdit(self)
+        self.input_by_user.hide()
 
         # Start Button
         self.btn_start = QPushButton("Start", self)
@@ -49,14 +58,17 @@ class QuickCount(QMainWindow):
         # Confirm Button
         self.btn_confirm = QPushButton("Confirm", self)
         self.btn_confirm.clicked.connect(self.confirm_answer)
+        self.btn_confirm.hide()
 
         # Next Button
         self.btn_next = QPushButton("Next", self)
         self.btn_next.clicked.connect(self.show_next_image)
+        self.btn_next.hide()
 
         # Close Button
         self.btn_close = QPushButton("Close", self)
         self.btn_close.clicked.connect(self.close_game)
+        self.btn_close.hide()
 
         # Layout
         layout = QVBoxLayout()
@@ -82,25 +94,36 @@ class QuickCount(QMainWindow):
     # start the game when the start button is clicked
     def start_game(self):
         self.btn_start.hide()
-        self.show_next_image_for_1sec()
+        self.show_next_image_for_short_seconds()
 
-    # show the image of current index for 1 second
-    def show_next_image_for_1sec(self):
-        self.lbl_answer_by_opencv.setText("")
-        self.input_by_user.setText("")
-        self.input_by_user.setDisabled(True)
+    # show the image of current index
+    def show_next_image_for_short_seconds(self):
+        self.lbl_answer_by_opencv.hide()
+        self.btn_next.hide()
+
+        self.lbl_image.show()
 
         if self.current_image_index < len(self.images):
             image = self.images[self.current_image_index]
             self.lbl_image.setPixmap(QPixmap(image))
 
-            # after 1 second, show the window for user input
-            self.timer.singleShot(1000, self.enter_count_by_user)
+            # after 1 second, then execute the function enter_count_by_user
+            self.timer.start(1000)
 
     # show the window for user input
     def enter_count_by_user(self):
-        self.input_by_user.setDisabled(False)
+        self.timer.stop()
+
+        self.lbl_image.hide()
+
+        self.lbl_input_by_user.show()
+        self.lbl_input_by_user.setText("Count by User: ")
+
+        self.input_by_user.show()
+        self.input_by_user.setText("")
         self.input_by_user.setFocus()
+
+        self.btn_confirm.show()
 
     # save the answer by user and show the image by OpenCV
     def confirm_answer(self):
@@ -113,43 +136,55 @@ class QuickCount(QMainWindow):
 
     # show the image by OpenCV
     def show_image_by_opencv(self):
-        self.lbl_answer_by_opencv.setText("")
-        self.input_by_user.setText("")
-        self.input_by_user.setDisabled(True)
+        self.lbl_input_by_user.hide()
+        self.input_by_user.hide()
+        self.btn_confirm.hide()
 
-        if self.current_image_index < len(self.images):
-            image = self.images[self.current_image_index]
-            counting_people = CountingPeople(image)
-            answer_by_opencv, image_by_opencv = counting_people.get_image_by_opencv()
-            self.answers_by_opencv.append(answer_by_opencv)
+        self.lbl_image.show()
+        self.lbl_answer_by_opencv.show()
+        self.btn_next.show()
 
-            # convert the numpy array to QImage
-            height, width, channel = image_by_opencv.shape
-            bytes_per_line = 3 * width
-            q_image = QImage(image_by_opencv.data, width, height, bytes_per_line, QImage.Format_RGB888)
+        image = self.images[self.current_image_index]
+        counting_people = CountingPeople(image)
+        answer_by_opencv, image_by_opencv = counting_people.get_image_by_opencv()
 
-            self.lbl_image.setPixmap(QPixmap(q_image))
-            self.lbl_answer_by_opencv.setText("Count by OpenCV: " + str(answer_by_opencv))
+        self.answers_by_opencv.append(answer_by_opencv)
 
-            self.current_image_index += 1
+        # convert the numpy array to QImage
+        height, width, channel = image_by_opencv.shape
+        bytes_per_line = 3 * width
+        q_image = QImage(image_by_opencv.data, width, height, bytes_per_line, QImage.Format_RGB888)
+
+        self.lbl_image.setPixmap(QPixmap(q_image))
+        self.lbl_answer_by_opencv.setText("Count by OpenCV: " + str(answer_by_opencv))
+
+        self.current_image_index += 1
 
     # show the next image
     def show_next_image(self):
         # if the game is not finished, show the next image
         if self.current_image_index < len(self.images):
-            self.show_next_image_for_1sec()
+            self.show_next_image_for_short_seconds()
         # if the game is finished, show the game result
         else:
             self.show_game_result()
 
     # show the game result (i.e., winner)
     def show_game_result(self):
+        self.lbl_image.hide()
+        self.lbl_answer_by_opencv.hide()
+        self.btn_next.hide()
+
+        self.lbl_result.show()
+        self.btn_close.show()
+
         score_by_user = sum(answer_by_user == answer
                             for answer_by_user, answer in zip(self.answers_by_user, self.answers))
         score_by_opencv = sum(answer_by_opencv == answer
                               for answer_by_opencv, answer in zip(self.answers_by_opencv, self.answers))
 
-        result = "User: " + str(score_by_user) + " vs. " + "OpenCV: " + str(score_by_opencv)
+        result = "User " + str(score_by_user) + " vs. " + "OpenCV " + str(score_by_opencv)
+
         if score_by_user > score_by_opencv:
             result += "\nUser wins!"
         elif score_by_user < score_by_opencv:
@@ -158,13 +193,6 @@ class QuickCount(QMainWindow):
             result += "\nDraw!"
 
         self.lbl_result.setText(result)
-
-        self.lbl_image.setPixmap(QPixmap(""))
-        self.lbl_answer_by_opencv.setText("")
-        self.btn_next.hide()
-
-        # show the close button
-        self.btn_close.show()
 
     # close the game window
     def close_game(self):
